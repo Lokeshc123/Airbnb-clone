17eca24ff6c35754b1465aa8fb5e90cacfc13811
Lokesh Chauhan <68843786+Lokeshc123@users.noreply.github.com>
2022-07-09 07:13:35 +0530

    SearchPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import Header from './Header';
-import Footer from './Footer';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
+
+    // BEM
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact  path= "/">
+            <Home />
+          </Route>
+          <Route path= "/search" >
+            <SearchPage/>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
